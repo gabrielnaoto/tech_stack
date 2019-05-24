72db6cae67dea2e02b80a5985934133a14c1593e
Gabriel <gabriel.naoto@jungsoft.com.br>
2019-05-24 15:18:27 -0300

    Create actions and animations

---
+++ src/components/ListItem.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableWithoutFeedback, LayoutAnimation } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection, Card } from './commons';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
+  render() {
+    const { id, title } = this.props.library;
+
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <Card>
+            <CardSection>
+              <Text style={styles.title}>
+                {title}
+              </Text>
+            </CardSection>
+            {this.renderDescription()}
+          </Card>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = {
+  title: {
+    fontSize: 18
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -18,7 +18,7 @@ const styles = {
     padding: 10
   },
   container: {
-    height: 60,
+    height: 90,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#ddd',
@@ -26,7 +26,8 @@ const styles = {
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     elevation: 2,
-    position: 'relative'
+    position: 'relative',
+    paddingTop: 30
   }
 };
 
